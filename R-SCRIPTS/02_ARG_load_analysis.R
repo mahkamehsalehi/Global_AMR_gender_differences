2c4c69c79b804d0bc323f751e40905a08c60a5e2
KatariinaParnanen <katariina.parnanen@gmail.com>
2024-09-20 15:39:40 +0300

    Exploratory fig

--- R-SCRIPTS/02_ARG_load_analysis.R
+++ R-SCRIPTS/02_ARG_load_analysis.R
@@ -8,6 +8,14 @@ plot_df <- df %>%
 # Create the plot, removing NA values from host_age_years
 ggplot(na.omit(plot_df), aes(x = host_age_years, y = log10_ARG_load, color = sex_combined)) +
   geom_jitter(size = 0.1, width = 0.1) +
+  geom_smooth(method = "lm") +
+  scale_x_continuous(breaks = c(0,1,3,5,10,15,20,25,30,40,50,60,70,80))+
+  theme_classic() +
+  facet_grid(cols=vars(category))
+
+
+ggplot(na.omit(plot_df), aes(x = host_age_years, y = log10_ARG_load, color = sex_combined)) +
+  geom_jitter(size = 0.1, width = 0.1) +
   geom_smooth(method = "loess") +
   scale_x_continuous(breaks = c(0,1,3,5,10,15,20,25,30,40,50,60,70,80))+
   theme_classic() +
