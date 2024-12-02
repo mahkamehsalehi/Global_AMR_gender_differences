fbc36e177541ae43a4a4cd12d061862aace93ec6
Mahkameh <mahkamehsalehi.93@gmail.com>
2024-12-02 13:26:27 +0200

    updated

--- R-SCRIPTS/SE_to_TSE.R
+++ R-SCRIPTS/SE_to_TSE.R
@@ -16,3 +16,18 @@ TSE_tree <- TreeSummarizedExperiment(
 )
 
 saveRDS(TSE_tree, "TSE.rds")
+
+
+tse_filtered <- readRDS("DATA/TSE_filtered.rds")
+
+tree_filtered <- rtree(n = nrow(tse_filtered))
+tree_filtered$tip.label <- rownames(tse_filtered)
+
+TSE_filtered_tree_ <- TreeSummarizedExperiment(
+  assays = assays(tse_filtered),
+  rowData = rowData(tse_filtered),
+  colData = colData(tse_filtered),
+  rowTree = tree_filtered
+)
+
+saveRDS(TSE_tree, "TSE_filtered.rds")
