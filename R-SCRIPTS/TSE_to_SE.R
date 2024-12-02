e5b4b8330b55438cc0f706890f73472ad88e0ddf
Mahkameh <mahkamehsalehi.93@gmail.com>
2024-12-02 11:17:24 +0200

    code to convert TSE to SE

---
+++ R-SCRIPTS/TSE_to_SE.R
@@ -0,0 +1,19 @@
+library(TreeSummarizedExperiment)
+library(SummarizedExperiment)
+
+tse <- readRDS("DATA/TSE_filtered.rds")
+# Extract the main components from TSE
+assay_data <- assays(tse)
+row_data <- rowData(tse)
+col_data <- colData(tse)
+
+# Create a SummarizedExperiment object
+se <- SummarizedExperiment(
+  assays = assay_data,
+  rowData = row_data,
+  colData = col_data
+)
+
+# Check the resulting SE object
+se
+saveRDS(se, file = "DATA/summarized_experiment.rds")
