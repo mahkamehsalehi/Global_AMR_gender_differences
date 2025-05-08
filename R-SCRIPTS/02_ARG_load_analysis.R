0195aff1ac80d1d92f7dffdccb455844440906a0
Mahkameh <mahkamehsalehi.93@gmail.com>
2025-05-08 14:00:25 +0300

    Update comments for clarity

--- R-SCRIPTS/02_ARG_load_analysis.R
+++ R-SCRIPTS/02_ARG_load_analysis.R
@@ -1,11 +1,20 @@
-TSE <- read_rds("DATA/TSE.rds")
+# Load necessary libraries
+library(jtools)
+library(tidyverse)
 
+
+# Load data from RDS file
+TSE <- read_rds("../DATA/TSE.rds")
+
+# Convert colData metadata to a data frame
 df <- colData(TSE) %>% as.data.frame()
 
-# Plot age gender by bioproject category
+# Subset relevant columns for plotting
 plot_df <- df %>%
   dplyr::select(matches(c("sex_combined", "log10_ARG_load", "host_age_years", "category", "World_Bank_Income_Group")))
-# Create the plot, removing NA values from host_age_years
+
+
+# --- PLOT 1: Age vs. ARG load (linear fit) faceted by income group ---
 ggplot(na.omit(plot_df), aes(x = host_age_years, y = log10_ARG_load, color = sex_combined)) +
   geom_jitter(size = 0.1, width = 0.1) +
   geom_smooth(method = "lm") +
@@ -14,8 +23,7 @@ ggplot(na.omit(plot_df), aes(x = host_age_years, y = log10_ARG_load, color = sex
   facet_grid(cols=vars(World_Bank_Income_Group))
 
 
-
-
+# --- PLOT 2: Age vs. ARG load (Loess fit) ---
 ggplot(na.omit(plot_df), aes(x = host_age_years, y = log10_ARG_load, color = sex_combined)) +
   geom_jitter(size = 0.1, width = 0.1) +
   geom_smooth(method = "loess") +
@@ -23,19 +31,28 @@ ggplot(na.omit(plot_df), aes(x = host_age_years, y = log10_ARG_load, color = sex
   theme_classic()
 
 
+# --- PLOT 3: ARG load vs. antibiotic use by gender (excluding certain data) ---
 df <- colData(TSE_gender) %>% as.data.frame()
-tmp<-df[df$category!="Infant Study",]
+
+# Filter out "Infant Study" category and Zimbabwe samples
+tmp <- df[df$category!="Infant Study",]
 tmp <- tmp[tmp$geo_loc_name_country_calc!="Zimbabwe",]
 ggplot(tmp, aes(x = Usage, y = log10_ARG_load, color = sex_combined)) +
   geom_jitter(size = 0.1, width = 0.1) +
   geom_smooth(method = "loess") +
   theme_classic() 
 
-library(jtools)
-fit <- aov(log10_ARG_load~category, data=df) 
 
-tmp<-df[df$category!="Infant Study",]
-tmp <- df[df$geo_loc_name_country_calc!="Zimbawe",]
+# --- ANOVA: ARG load by study category ---
+fit <- aov(log10_ARG_load ~ category, data = df) 
+
+
+# ---GLM: ARG diversity by gender ---
+tmp <- df %>%
+  filter(category != "Infant Study",
+         geo_loc_name_country_calc != "Zimbabwe")
+
+
+# Fit GLM and summarize (exponentiate coefficients)
 glm(ARG_div ~ sex_combined, data = df) %>% 
   summ(exp = TRUE, digits = 3)
-
